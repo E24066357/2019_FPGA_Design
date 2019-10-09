1c4034848553942f1baa0ef8c2085af7f7b6b162
E24066357 <47938141+E24066357@users.noreply.github.com>
2019-10-09 16:21:39 +0800

    Update PWM_Decoder.v

--- Lab02/Lab2-1/src/PWM_Decoder.v
+++ Lab02/Lab2-1/src/PWM_Decoder.v
@@ -1,38 +1,89 @@
 module PWM_Decoder (
-  input  [1:0] sw,
+ 
   output reg [7:0] R_time_out,
-  output reg [7:0] G_time_out,
-  output reg [7:0] B_time_out
+  reg [3:0] nstate;
+  reg [3:0] cstate;
+  parameter add=2'd0,sub=2'd1,
 );
 
-  always @ ( * ) begin
-    case (sw)
-      2'b00: begin
-        R_time_out = 8'd100;
-        G_time_out = 8'd0;
-        B_time_out = 8'd0;
-      end
-      2'b01: begin
-        R_time_out = 8'd100;
-        G_time_out = 8'd100;
-        B_time_out = 8'd0;
-      end
-      2'b10: begin
-        R_time_out = 8'd100;
-        G_time_out = 8'd10;
-        B_time_out = 8'd255;
-      end
-      2'b11: begin
-        R_time_out = 8'd255;
-        G_time_out = 8'd255;
-        B_time_out = 8'd255;
-      end
-      default: begin
-        R_time_out = 8'd0;
-        G_time_out = 8'd0;
-        B_time_out = 8'd0;
-      end
-    endcase
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+        R_time_out<=0;
+        nstate<=add;
+        else
+          cstate<=nstate;
+    end
+  
+  
+  
+ always@(posedge clk or posedge rst)
+    begin
+      case(cstate)
+        add:
+          begin
+            R_time_out=(R_time_out==8'd254)?8'd255:R_time_out+1;
+          end
+        sub:
+          begin
+            R_time_out = (R_time_out == 8'd1)? 8'd0 : R_time_out - 8'd1;
+          end
+        
+       
+    end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+always @ ( * ) begin
+  //////////
+  case(cstate)
+   add:
+      begin
+      
+
+    if(R_time_out==8'd255)
+    begin
+    nstate=sub;
+    end
+        
+    else 
+    begin
+    nstate=add; 
+    end 
+      
+      
   end
+    //////////
+    sub:
+    begin
+   
+      if(R_time_out==8'd0)
+    begin
+    nstate=add;
+    end
+        
+    else 
+    begin
+    nstate=sub; 
+    end  
+      
+      
+      
+    end
+    
+    
+  endcase
+    
+    
+    
+  end//*號給狀態
 
 endmodule // Decoder
